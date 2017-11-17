f640834c3e8f213c187265180f5772ffa0003f45
Eleni A <eleni.arvanitis@me.com>
2017-11-17 11:37:25 -0500

    playing with fire

--- App.jsx
+++ App.jsx
@@ -1,3 +1,32 @@
-import './public/app';
+import React, { Component } from 'react';
+import fire from './public/app';
 
-export default () => '🔥 Ready.'
+// export default () => '🔥 Ready.'
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    // Get elements
+    const pObj = document.getElementById('object');
+
+    // Create references
+      // reference refers to the root of the database
+      // child adds a child to the object root
+    const db = fire.database().ref('/').child('object');
+    console.log('db:',fire.database().ref('coquill-e559a'));
+
+    // Sync object changes
+    db.on('value', snap => console.log('hello?',snap.val()));
+  }
+
+  render() {
+    return (
+      <div>
+        <p> 🔥 Ready. </p>
+      </div>
+    );
+  }
+}
