6cbccd05bb057fdc81da4efd2ffa35b0c6173624
Eleni A <eleni.arvanitis@me.com>
2017-11-21 17:50:00 -0500

    getting rid of other things we dont need

--- client/store/index.js
+++ client/store/index.js
@@ -7,12 +7,11 @@ import fire from '~/public/secrets';
 
 import firebase from 'firebase';
 
-
 const firebaseMiddleware = store => next => {
   const ref = firebase.database().ref().child('screenplay');
   const listener = ref.on('child_added', snap => next(snap.val()));
   // this.unsubscribe = () => ref.off('child_added', listener)
-  
+
   return action => {
     if (action.doNotSync) { return next(action) }
     const actionKey = ref.push().key;
@@ -20,9 +19,9 @@ const firebaseMiddleware = store => next => {
     return ref.child(actionKey).set({actionKey, ...action})
   }
 };
-/* 
+/*
 // attach key here
-      =>  
+      =>
 // hold state in store, create store in screenplay component
 */
 
@@ -35,26 +34,6 @@ const store = createStore(
     )
   )
 );
-window.store = store
 
-//ex : add char in to firebase
-export const addChar = (name) => {
-    //firebase ref, check if child exist, then add 
-    const ref = fire.database().ref().child("screenplay")
-    // screenplays/screenplay1/actions
-    
-    ref.once('value', snap => {
-      if (snap.hasChild('characters')) {
-        snap.ref.push({name})
-      } else {
-        //makek new char child
-        // ref.on("child_added", snap => {
-        //   console.log("snap", snap)
-        //   snap.ref().add()
-        // })
-      }
-    });
-}
-  
 export default store
 export * from './reducer'
