c80f60fcaad1413ead0db914256a46c1d741cc01
Eleni A <eleni.arvanitis@me.com>
2017-11-22 12:10:41 -0500

    fixed and merged editor with app

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducer'
 import fire from '~/public/secrets';
 
 const firebaseMiddleware = store => next => {
   const ref = fire.database().ref().child('screenplay');
-  const listener = ref.on('child_added', snap => next(snap.val()));
+  // const listener = ref.on('child_added', snap => next(snap.val()));
   // this.unsubscribe = () => ref.off('child_added', listener)
 
   return action => {
@@ -33,5 +33,6 @@ const store = createStore(
   )
 );
 
+window.store = store;
 export default store
 export * from './reducer'
