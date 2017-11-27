c43c9bb07c047dea104bb75c2ecd4c6cf300d6b8
Eleni A <eleni.arvanitis@me.com>
2017-11-27 12:01:40 -0500

    added BrowserRouter

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import Routes from './client/routes'
 import { render } from 'react-dom'
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Editor from '~/client/components/Editor';
-
-// import { BrowserRouter as Router } from 'react-router-dom';
 
 function main() {
   render(
-  <AppContainer>
-    <Editor />
-    {/*<Routes />*/}
-  </AppContainer>,
-  document.getElementById('main'))
+    <AppContainer>
+      <Router>
+        <Editor />
+      </Router>
+    </AppContainer>,
+    document.getElementById('main')
+  )
 }
 
 main();
