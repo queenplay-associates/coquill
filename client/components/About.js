d308d00fc9fc4037c655e524068a358252f32cca
Eleni A <eleni.arvanitis@me.com>
2017-11-29 12:43:37 -0500

    fixed some css imports

--- client/components/About.js
+++ client/components/About.js
@@ -8,7 +8,7 @@ const carouselImages = [
   'https://quilljs.com/assets/images/features/scale.svg'
 ];
 
-import '~public/assets/About.css'
+import '~/public/assets/About.css'
 
 export default () =>
   <div>
