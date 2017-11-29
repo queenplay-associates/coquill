ad922b907e4e4c4057290c57b7f17eaa626e2356
cyng24 <cyng24@gmail.com>
2017-11-28 19:30:28 -0500

    commented out the broken screenplays component

--- App.jsx
+++ App.jsx
@@ -18,7 +18,7 @@ export default () =>
           <Route exact path='/' component={() =>
             <Editor fireRef={db.ref('screenplays').child('welcome')}/>
           }/>
-          <Route path='/screenplays' component={Screenplays}/>
+          {/* <Route path='/screenplays' component={Screenplays}/> */}
           <Route exact path='/screenplays/:screenplayId' component={
             ({match: {params: {screenplayId}}}) =>
               <Editor fireRef={db.ref('screenplays').child(screenplayId)}/>
