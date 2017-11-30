931fd86e9a74a0bdd78fd15fdc9bdc32d2767e7e
Sam Zhang <samanthazhang9@gmail.com>
2017-11-30 13:52:02 -0500

    screenplays rendering but not loading

--- client/components/Screenplays.js
+++ client/components/Screenplays.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
-// import { db } from '~/public/secrets'
 import '~/public/assets/Buttons.css';
 import { Link } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 import '~/public/assets/Screenplays.css'
+
+import { db } from '~/public/secrets'
 // grab screenplays from database
   // list id, title, owners, short description (?)
   // make a func to get rid of last comma of authors array
   // what about anon users?
   // grab elements properly (map prob won't work here)
 
+    // db.ref('screenplays').map(screenplay =>
+    //   <div>
+    //     <h3>{`${screenplay.id}. ${screenplay.title.toUpperCase()}`}</h3>
+    //     <h6>{`Screenplay by ${screenplay.authors.join(', ').trim()}`}</h6>
+    //   </div>
+    // )
+
 export default class Screenplays extends Component {
   constructor() {
     super()
     this.state = {
-      value: ''
+      value: '',
+      screenplays: []
     }
+  }
+
+  componentDidMount() {
+    let arr = []
+    db.ref('screenplays').on('child_added', snapshot => {
+     arr.push(snapshot.key)
+    })
+
+    this.setState({screenplays: arr})
+    console.log('SCREENPLAYS IN COMPONENTDIDMOUNT', this.state.screenplays)
   }
 
   handleChange = evt => {
@@ -34,12 +53,12 @@ export default class Screenplays extends Component {
     return <div className = "demo">
         <h3>List of Screenplays</h3>
         <ul>
-          <li><Link to='/screenplays/hop'>Hop</Link></li>
-          <li><Link to='/screenplays/her'>Her</Link></li>
+          {console.log('SCREENPLAYS IN RENDER', this.state.screenplays)}
+          {this.state.screenplays.map(name => <li>{name}</li>)}
           <li>
             <form onSubmit={this.handleSubmit}>
               <input className='input' onChange={this.handleChange} value={this.state.value} />
-            <button type='button' onClick={this.handleSubmit}>Create</button></form>
+            <button type='submit'>Create</button></form>
           </li>
         </ul>
       </div>
