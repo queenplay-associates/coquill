8eb8f2bca630d0b90e313166ccf9dca1b2c23aac
Eleni A <eleni.arvanitis@me.com>
2017-12-01 15:49:14 -0500

    refactor

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import firebase from 'firebase'
 import { checkUser } from './authHelper'
 
-//TODO: reduce ref to a var and resue it 
+//TODO: reduce ref to a var and reuse it
 
 export default class Auth extends Component {
     constructor(props) {
@@ -11,8 +11,10 @@ export default class Auth extends Component {
         const { userName, userFace } = props;
         this.state = { userName, userFace };
     }
-    componentDidMount(){
-        this.props.db.ref('users').on('value', snapshot => {         
+
+    componentDidMount() {
+        this.props.db.ref('users').on('value', snap => {
+          // we're not doing anything here yet
         })
     }
 
@@ -22,82 +24,86 @@ export default class Auth extends Component {
 
         firebase.auth().signInWithPopup(provider).then(result => {
             const token = result.credential.accessToken;
-            const user = result.user;
-            return user
+            return result.user;
         })
         .then(user => {
-            let email = user.email
-            let bool = false    //FIXME: change name    
-            let ref ;    
-            firebase.database().ref('users').once("value", snapshot => {
-                ref = snapshot.ref
-                for (const prop in snapshot.val()) {
-                    let existatedEmail = snapshot.val()[prop].email
-                    if (existatedEmail === email) {
-                        bool = true
-                        break
-                        }
-                    }
-                 })
-                return [bool, user, ref]
-             })
-          .then(data => {
-            if (!data[0]) regiesterUser(data[1], data[2]) // refactor this check if user exists or not then add to db
-        }).catch()
+            let email = user.email,
+                registeredUser = false,
+                ref;
 
-        const regiesterUser = (data, ref) => {
+            firebase.database().ref('users').once("value", snap => {
+                ref = snap.ref
+
+                for (const prop in snap.val()) {
+                    let emailExists = snap.val()[prop].email
+
+                    if (emailExists === email) {
+                        registeredUser = true
+                        break
+                    }
+                }
+            })
+
+            return [registeredUser, user, ref]
+        })
+          .then(data => {
+            if (!data[0]) registerUser(data[1], data[2])
+            // refactor above if statement to check whether user exists then add to db
+          })
+          .catch(err => console.error(err))
+
+        const registerUser = (data, ref) => {
             const { email, displayName, photoURL, uid } = data
             const permissions = [
                 { id: 'blah', access: 'read' },
                 { id: 'screen', access: 'admin' }
             ]
-            ref.update({[uid]: {
+
+            ref.update({
+              [uid]: {
                 email,
                 displayName,
                 photoURL,
                 permissions
-            } })
+            }})
         }
     }
 
-    handleAnonymousClick =evt => {
+    handleAnonymousClick = evt => {
         evt.preventDefault()
         //FIXME: this can be a promise thing
-        firebase.auth().signInAnonymously().then(
-            firebase.auth().onAuthStateChanged(function (user) {
+        firebase.auth().signInAnonymously()
+          .then(firebase.auth().onAuthStateChanged(user => {
                 let isAnonymous, uid;
+
                 user
                     ? (isAnonymous = user.isAnonymous,
                         uid = user.uid)
-                    :
-                    console.log("user singed out")
-            })
-        ).catch(function (error) {
-            console.log(error.code, error.message)
-        })
+                    : console.log("user signed out")
+          }))
+          .catch(err => console.log(err.code, err.message))
     }
 
     handleSignOutClick = evt => {
         evt.preventDefault()
-        firebase.auth().signOut().then(() => {
+        firebase.auth().signOut()
+          .then(() => {
             this.setState({ userName: "Stranger", userFace: "" })
-        }, error => {
-            //console.error('Sign Out Error', error);
-        });
+          })
+          .catch(err => console.error('Anon sign out error', err));
     }
 
     render() {
-        const { status } = this.props
-        const { userName, userFace } = this.state
-        return (<div className="Auth">
-            <p>Auth Page Hello </p>
-            <h1>{userName}</h1>
-            <button onClick={this.handleAnonymousClick}> anonymous-auth 🤣 </button>
-            <button onClick={this.handleGoogleClick}> google auth 🤣 </button>
-            <button onClick={this.handleSignOutClick}> sign out 🤣 </button>
-            <h1>🤷🏻‍{userName}</h1>
-            <img className="userFace" src={userFace} />
-        </div>
-        )
+        const { status } = this.props,
+              { userName, userFace } = this.state
+
+        return <div className="Auth">
+              <p>Auth Page Hello</p>
+              <h1>🤷🏻‍♀️‍{userName}</h1>
+              <button onClick={this.handleAnonymousClick}> anonymous-auth 🤣 </button>
+              <button onClick={this.handleGoogleClick}> google auth 🤣 </button>
+              <button onClick={this.handleSignOutClick}> sign out 🤣 </button>
+              <img className="userFace" src={userFace} />
+          </div>
     }
 }
