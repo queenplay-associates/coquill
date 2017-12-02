f21c4e32f1a493746adff69501a2de2c566f0327
Eleni A <eleni.arvanitis@me.com>
2017-12-02 14:17:35 -0500

    moving around things for our needs

---
+++ client/components/Anon.js
@@ -0,0 +1,24 @@
+const anonAnimalsList = [
+  'Alligator', 'Anteater', 'Armadillo', 'Auroch', 'Axolotl',
+  'Badger', 'Bat', 'Beaver', 'Buffalo', 'Camel', 'Capybara',
+  'Chameleon', 'Cheetah', 'Chinchilla', 'Chipmunk', 'Chupacabra',
+  'Cormorant', 'Coyote', 'Crow', 'Dingo', 'Dinosaur', 'Dolphin',
+  'Duck', 'Elephant', 'Ferret', 'Fox', 'Frog', 'Giraffe', 'Gopher',
+  'Grizzly', 'Hedgehog', 'Hippo', 'Hyena', 'Ibex', 'Ifrit', 'Iguana',
+  'Jackal', 'Kangaroo', 'Koala', 'Kraken', 'Lemur', 'Leopard',
+  'Liger', 'Llama', 'Manatee', 'Mink', 'Monkey', 'Moose', 'Narwhal',
+  'Nyan Cat', 'Orangutan', 'Otter', 'Panda', 'Penguin', 'Platypus',
+  'Pumpkin', 'Python', 'Quagga', 'Rabbit', 'Raccoon', 'Rhino',
+  'Sheep', 'Shrew', 'Skunk', 'Squirrel', 'Tiger', 'Turtle', 'Walrus',
+  'Wolf', 'Wolverine', 'Wombat'
+];
+
+// if a user is anonymous
+// make the user image an anonymous animal
+  // (grab from /public/assets/anonymous-animals/icons')
+// pick a random animal, corresponding to its image
+//
+
+/*
+<ul class="animal-list"><li class="animal-item"><img src="icons/Alligator.png" alt="Alligator" title="Alligator"></li>
+*/
