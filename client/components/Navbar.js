f15d6060a235835de6f80034b08e47164614a169
Eleni A <eleni.arvanitis@me.com>
2017-12-02 15:57:30 -0500

    removed an empty line, adding component to navlink screenplays doesnt fix that issue

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,7 +5,6 @@ import '~/public/assets/Homepage.css'
 
 const logo = 'https://img00.deviantart.net/3b69/i/2015/125/6/f/burnt_quill_s_cutie_mark_final_version_by_burntquill-d8j1is9.png';
 
-
 export default class Navbar extends Component {
   render() {
     const { userName } = this.props;
