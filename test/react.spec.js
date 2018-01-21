e88115c3470ebf63f2e12cc97cc8dd4cc30c9db2
guangLess <bha.guang@gmail.com>
2018-01-20 23:07:30 -0500

    set up jest

--- test/react.spec.js
+++ test/react.spec.js
@@ -1,53 +1,8 @@
-// import React from 'react'
-// import {shallow} from 'enzyme'
-// import {spy} from 'sinon'
-// import {expect} from 'chat'
 
-// import Script from '~/client/components/Script'
+function sum(a, b) {
+    return a + b;
+  }
 
-import * as reduxs from '~/client/store/reducer';
-
-/*
-    SceneHeading,
-    Action,
-    Character,
-    Parenthetical,
-    Dialogue,
-    Shot,
-    Transition,
-    Text
-
-
-describe('Script React components', () => {
-    let selectedScript, scriptComponent;
-    let scripts = ['SceneHeading', 'Action', 'Dialogue', 'Transition', 'Shot', 'Transition']
-    function getRandomScript () {
-        return scripts[Math.floor() * 5]
-    }
-    beforeEach('Create component', () => {
-        selectedScript = getRandomScript()
-        scriptComponent = shallow(<Script />)
-    })
-    it('returns a script components', () => {
-        //expect(scriptComponent.props.key.to.be.equal(selectedScript))
-        //expect(scriptComponent.prototype.componenetDidMount.calledOnce).to.equal(true) 
-    })
-})
-*/
-
-describe('actions', () => {
-    it('should create an action SET_VALUE to text content', () => {
-        const value = 'screen play',
-        componentKey = 'px123pxpx',
-              name = 'nori dog'
-
-        const expectedAction = {
-            type: reduxs.SET_VALUE,
-            value,
-            componentKey,
-            name 
-        }
-
-        expect(reduxs.setValue(value, componentKey, name).toEqual(expectedAction))
-    })
-}) 
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
