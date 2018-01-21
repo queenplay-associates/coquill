a83b0e1c9d62f767389b82d9c55546c173535483
guangLess <bha.guang@gmail.com>
2018-01-21 17:48:13 -0500

    test Link

--- test/react.spec.js
+++ test/react.spec.js
@@ -10,14 +10,30 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Enzyme,{ shallow, mpunt } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 import { Link } from 'react-router-dom';
 import { MemoryRouter } from 'react-router'
 import About from '../client/components/About'
 
-test('<About /> component', () => { 
+Enzyme.configure({ adapter: new Adapter()});
+//FIXME: takout __mock__ stuff. 🙄
+
+describe('<About /> component', () => {
+  
   const about = renderer.create(<MemoryRouter>
     <About />  
   </MemoryRouter>)
-  let tree = about.toJSON()
-  expect(tree).toMatchSnapshot()
+
+  test('snapshop of the DOM', () => { 
+    let tree = about.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('links <Link to />', () => {
+    let wrapper = shallow(<About />)
+    //let linka = wrapper.find(Link) //.find({to: '/screenplays'})
+    //console.log(linka)
+    expect(wrapper.find(Link).length).toBe(1);
+  })
 })
